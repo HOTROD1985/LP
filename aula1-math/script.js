9d93c98a1fc0ca366fe24b745fb1efee8a1d6cba
HOTROD1985 <pietro.ferrari@sounotredame.org.br>
2024-09-02 08:45:46 -0300

    aula de math

---
+++ aula1-math/script.js
@@ -0,0 +1,37 @@
+let numero = 4.7
+
+
+//Esse round ele arredonda comun  
+console.log(Math.round(numero))
+
+//O math ele smeore arredonda pra cima 
+console.log(Math.ceil(numero))
+
+//Nesse caso ele se arrendondara para baixo 
+console.log(Math.floor(numero))
+
+
+//Aqui nesse caso ele ira se cria uma poencia 
+console.log(Math.pow(2,3))
+
+//Osqrt ele ira se gera a raiz quadrada 
+console.log(Math.sqrt(35))
+
+//Ele pega o numero e transforma ele em positivo
+console.log(Math.abs(-6))
+
+//Ele acha qual é o numero mais baixo  
+console.log(Math.min(7,45, 89, 99, 123))
+
+//Ele ach o numero mmaior 
+console.log(Math.max(7, 45, 89, 99, 123))
+
+let  numeroAleatorio = Math.floor (Math.random () * 10)
+console.log(numeroAleatorio)
+
+const numero1 = prompt("Digite um numero")
+const numero2 = prompt("Digite um numero")
+
+let media = (numero1 + numero2) / 2 
+
+console.log('A media é:                  ')
